eae94280c9f3fb4d95e59169e35e91af9e1bea2a
Aleksey Kuznetsov <keenflake@gmail.com>
2022-08-21 17:09:14 +0400

    Fix Next.js meta warning

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,16 @@
 import { AppProps } from 'next/app';
+import Head from 'next/head';
 import { FC } from 'react';
 
 import '@app/styles/index.css';
 
-const App: FC<AppProps> = ({ Component, pageProps }) => <Component {...pageProps} />;
+const App: FC<AppProps> = ({ Component, pageProps }) => (
+  <>
+    <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+    </Head>
+    <Component {...pageProps} />
+  </>
+);
 
 export default App;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,6 @@ import { FC } from 'react';
 const Document: FC = () => (
   <Html>
     <Head>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
       <meta name="theme-color" content="#ffffff" />
 
       {/* Typeface */}
