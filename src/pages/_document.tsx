0274b26290f5fbd5197df6406292398fa69e2c06
Aleksey Kuznetsov <keenflake@gmail.com>
2022-08-21 17:07:31 +0400

    Add favicons and `manifest.json`

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,18 @@ import { FC } from 'react';
 const Document: FC = () => (
   <Html>
     <Head>
-      <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500&display=swap" rel="stylesheet" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+      <meta name="theme-color" content="#ffffff" />
+
+      {/* Typeface */}
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500&display=swap" />
+
+      {/* Icons */}
+      <link rel="icon" href="/icons/favicon.svg" />
+      <link rel="mask-icon" href="/icons/mask-icon.svg" color="#527fb8" />
+      <link rel="apple-touch-icon" href="/icons/apple-touch-icon.png" />
+
+      <link rel="manifest" href="/manifest.json" />
     </Head>
     <body>
       <Main />
